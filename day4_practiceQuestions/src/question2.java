3b1ec7464af63ac8ff7afde56103cd47e8c81706
Mahtab <mahtaba515@gmail.com>
2025-10-15 15:03:08 +0530

    practice question2 in day4 practice questions

---
+++ day4_practiceQuestions/src/question2.java
@@ -0,0 +1,12 @@
+public class question2 {
+    public static void main(String[] args) {
+        //Question 2 : Finish the following code so that it prints You have a fever if your temperature
+        //is above 100 and otherwise prints You don't have a fever.
+        double temp = 103.5;
+        if (temp > 100){
+            System.out.println("You have a fever");
+        }else{
+            System.out.println("You don't have a fever");
+        }
+    }
+}
