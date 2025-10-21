0dd4fe5a36c0958c17644f29f47fbaa8ba79b991
Mahtab <mahtaba515@gmail.com>
2025-10-21 14:20:49 +0530

    Loop

---
+++ Day5_loops/src/LoopPractice1.java
@@ -0,0 +1,8 @@
+public class LoopPractice1 {
+    public static void main(String[] args) {
+//        Print SQUARE Pattern
+        for (int i = 1; i <= 4; i++){
+            System.out.println("****");
+        }
+    }
+}
